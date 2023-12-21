a63ab7d574c53631810ccb37481daabadc67b72e
richardenrico <richardenrico02@gmail.com>
2023-12-21 23:30:19 +0800

    feat: add some color

--- src/shared/utils/colors.ts
+++ src/shared/utils/colors.ts
@@ -1,5 +1,18 @@
-export type Colors = 'white';
+export type Colors =
+    | 'white'
+    | 'desaturated-red'
+    | 'soft-red'
+    | 'dark-grayish-red'
+    | 'white-red'
+    | 'peach'
+    | 'peach-red'
 
 export const COLORS: Record<Colors, string> = {
     white: '#FFFFFF',
-};
+    'desaturated-red': '#ce9797',
+    'soft-red': '#f96262',
+    'dark-grayish-red': '#413a3a',
+    "white-red": '#fff5f5',
+    peach: '#f8bfbf',
+    "peach-red": '#ee8c8c',
+}
