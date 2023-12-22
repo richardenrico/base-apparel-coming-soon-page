702f158c606601ebf2a815e9293bf263a12e180e
richardenrico <richardenrico02@gmail.com>
2023-12-22 19:43:40 +0800

    feat: add form validation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -10,15 +10,34 @@ import {
     getGradient,
     useMantineTheme,
 } from '@mantine/core'
+import { useForm, zodResolver } from '@mantine/form'
 import Seo from 'shared/components/Seo'
+import z from 'zod'
 import HeroDesktop from 'assets/images/hero-desktop.jpg'
 import BgPatternDesktop from 'assets/images/bg-pattern-desktop.svg'
 import Logo from 'assets/images/logo.svg'
 import IconArrow from 'assets/images/icon-arrow.svg'
 import IconError from 'assets/images/icon-error.svg'
+import { useState } from 'react'
 
 const Home = () => {
     const theme = useMantineTheme()
+
+    const [isSubmitted, setIsSubmitted] = useState<boolean>(false)
+
+    const schema = z.object({
+        email: z.string().email(),
+    })
+
+    const form = useForm({
+        initialValues: {
+            email: '',
+        },
+        validate: zodResolver(schema),
+    })
+
+    console.log(form.values.email);
+    
 
     return (
         <>
@@ -72,58 +91,91 @@ const Home = () => {
                                     email below to stay up-to-date with
                                     announcements and our launch deals.
                                 </Text>
-                                <Group mt={50} className="w-full relative">
-                                    <TextInput
-                                        className="w-[90%] absolute"
-                                        placeholder="Email Address"
-                                        radius="xl"
-                                        size="lg"
-                                        rightSection={
-                                            true && <Image src={IconError} />
-                                        }
-                                        error={true}
-                                        styles={{
-                                            wrapper: {
-                                                '--_input-placeholder-color':
-                                                    '#debaba',
-                                                fontWeight: 300,
-                                                '--_input-bd': '#debaba',
-                                                '--_input-padding-left': '7%',
-                                                // 25% when error 15% when not
-                                                '--_input-padding-right': true
-                                                    ? '25%'
-                                                    : '15%',
-                                                '--_input-bd-focus': '#ce9797',
-                                                color: 'black',
-                                            },
-                                            input: {
-                                                backgroundColor: 'transparent',
-                                                fontSize: 16,
-                                            },
-                                            section: {
-                                                marginRight: '12%',
-                                            },
-                                        }}
-                                    />
-                                    <Button
-                                        className="absolute w-1/5 right-0 shadow-2xl"
-                                        radius="xl"
-                                        size="lg"
-                                        // mb={19}
-                                        styles={{
-                                            root: {
-                                                '--button-bg':
-                                                    'linear-gradient(135deg, #f8bfbf 0%, #ee8c8c 100%)',
-                                                '--button-hover': '#f9cfd0',
-                                            },
-                                        }}
-                                    >
-                                        <Image src={IconArrow} />
-                                    </Button>
-                                </Group>
-                                {true && (
+                                <form
+                                    onSubmit={(event) => {
+                                        setIsSubmitted(true)
+                                        event.preventDefault()
+                                    }}
+                                    onChange={() => setIsSubmitted(false)}
+                                >
+                                    <Group mt={50} className="w-full relative">
+                                        <TextInput
+                                            className="w-[90%] absolute"
+                                            placeholder="Email Address"
+                                            radius="xl"
+                                            size="lg"
+                                            rightSection={
+                                                isSubmitted &&
+                                                !form.isValid('email') && (
+                                                    <Image src={IconError} />
+                                                )
+                                            }
+                                            error={
+                                                isSubmitted &&
+                                                !form.isValid('email')
+                                            }
+                                            styles={{
+                                                wrapper: {
+                                                    '--_input-placeholder-color':
+                                                        '#debaba',
+                                                    fontWeight: 300,
+                                                    '--_input-bd': '#debaba',
+                                                    '--_input-padding-left':
+                                                        '7%',
+                                                    '--_input-padding-right':
+                                                        !form.isValid('email')
+                                                            ? '25%'
+                                                            : '15%',
+                                                    '--_input-bd-focus':
+                                                        '#ce9797',
+                                                    color: 'black',
+                                                },
+                                                input: {
+                                                    backgroundColor:
+                                                        'transparent',
+                                                    fontSize: 16,
+                                                },
+                                                section: {
+                                                    marginRight: '12%',
+                                                },
+                                                error: {
+                                                    fontSize: 14,
+                                                },
+                                            }}
+                                            {...form.getInputProps('email', {
+                                                withError: false,
+                                            })}
+                                        />
+                                        <Button
+                                            className="absolute w-1/5 right-0 shadow-2xl"
+                                            radius="xl"
+                                            size="lg"
+                                            type="submit"
+                                            // mb={16}
+                                            styles={{
+                                                root: {
+                                                    '--button-bg':
+                                                        'linear-gradient(135deg, #f8bfbf 0%, #ee8c8c 100%)',
+                                                    '--button-hover': '#f9cfd0',
+                                                },
+                                            }}
+                                        >
+                                            <Image src={IconArrow} />
+                                        </Button>
+                                    </Group>
+                                </form>
+                                {isSubmitted && !form.isValid('email') ? (
                                     <Text mt={'md'} fz={14} ml={'6%'} c={'red'}>
                                         Please provide a valid email
+                                    </Text>
+                                ) : (
+                                    <Text
+                                        mt={'md'}
+                                        fz={14}
+                                        ml={'6%'}
+                                        c={'transparent'}
+                                    >
+                                        You see nothing okay
                                     </Text>
                                 )}
                             </Stack>
